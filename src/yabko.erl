dc060e8f7663841001871fa1f363160669052112
Guilherme Andrade <g@gandrade.net>
2017-10-11 01:10:37 +0100

    Add float32 test coverage of binary decoder

--- src/yabko.erl
+++ src/yabko.erl
@@ -82,6 +82,8 @@ bin_unicode_decode_test() ->
 xml_unicode_decode_test() ->
     run_test__("test_data/unicode.xml.plist", {ok, expected_unicode_test_data()}).
 
+float32_decode_test() ->
+    run_test__("test_data/float32.bin.plist", {ok, #{<<"etc etc..">> => 1.0}}).
 
 run_test__(Path, ExpectedResult) ->
     {ok, Encoded} = file:read_file(Path),
